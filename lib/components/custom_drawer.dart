e6ca6fe6178f97f5dd5c90cff9874e208b2075dd
Yefta Steven Marcellius <yeftasm@gmail.com>
2024-10-27 21:46:52 +0700

    Fix bug

--- lib/components/custom_drawer.dart
+++ lib/components/custom_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uasppb_2021130007/auth/login_or_register.dart';
 import 'package:uasppb_2021130007/components/custom_drawer_tile.dart';
-import 'package:uasppb_2021130007/pages/login_page.dart';
 import 'package:uasppb_2021130007/pages/settings_page.dart';
 
 class CustomDrawer extends StatelessWidget {
